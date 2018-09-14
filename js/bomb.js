c88a43d42929e949d6cc83fe97ee2161bbc5ecc9
Henry H <iisdndr@gmail.com>
2018-09-14 12:42:17 -0700

    Finished UI layout

--- js/bomb.js
+++ js/bomb.js
@@ -1,4 +1,8 @@
-console.log("javascript running");
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/ 
+/*----- event listeners -----*/
+/*----- functions -----*/
 
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM loaded");
